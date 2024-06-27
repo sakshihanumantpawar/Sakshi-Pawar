c1e96f424d4ff3e25f7f48c07ba9c072605e4b2c
name <112334150+sakshihanumantpawar@users.noreply.github.com>
2024-06-27 22:17:00 +0530

    TNS code

---
+++ java2/src/Defaul/TNS1.java
@@ -0,0 +1,51 @@
+package Defaul;
+
+public class TNS1 {
+
+	public static void main(String[] args) {
+		
+
+				short a=2;
+				int c=10;
+				byte b=6;
+				long l=1253621;
+				System.out.println(a);
+				System.out.println(c);
+				System.out.println(b);
+				System.out.println(l);
+				//float data type
+				float f=65.20298f;
+				double d=876.765d;
+				System.out.println(f);
+				System.out.println(d);
+				//boolean data type
+				boolean g= true;
+				System.out.println(g);
+				
+
+				
+				char var1=65;
+				char var2='b';
+				System.out.println(var1);
+				System.out.println(var2);
+				int VAr='A';
+				System.out.println(b);
+				System.out.println("the value of the char data type is "+var2);
+				System.out.println("the 65 is the asci value of "+var1);
+				
+				
+				int i=3;
+				System.out.println(i);
+				System.out.println(i++);
+				System.out.println(i);
+				System.out.println(++i);
+				
+				
+
+			}
+
+		
+
+	}
+
+
---
+++ java2/src/Defaul/java1.java
@@ -0,0 +1,46 @@
+package Defaul;
+
+public class java1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short a=2;
+		int c=10;
+		byte b=6;
+		long l=1253621;
+		System.out.println(a);
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(l);*/
+		//float data type
+		float f=65.20298f;
+		double d=876.765d;
+		System.out.println(f);
+		System.out.println(d);
+		//boolean data type
+		boolean g= true;
+		System.out.println(g);
+		
+
+		
+		char var1=65;
+		char var2='b';
+		System.out.println(var1);
+		System.out.println(var2);
+		int b='A';
+		System.out.println(b);
+		System.out.println("the value of the char data type is "+var2);
+		System.out.println("the 65 is the asci value of "+var1);
+		
+		
+		int i=3;
+		System.out.println(i);
+		System.out.println(i++);
+		System.out.println(i);
+		System.out.println(++i);
+		
+		
+
+	}
+
+}
